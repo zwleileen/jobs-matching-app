fa515dc7eaaf3ae6a7ada7e944a867d57fc259e2
eileen <zwleileen@gmail.com>
2025-01-24 18:35:28 +0800

    formatted navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 // src/App.jsx
 import './App.css'
+import { Routes } from "react-router-dom";
+import NavBar from './components/NavBar/NavBar';
 
 const App = () => {
 
   return (
   <>
-  <h1>Hello world!</h1>
+  <h1>Jobs Matching App</h1>
+  <NavBar/>
+  {/* <Routes>
+  </Routes> */}
   </>
   );
 };
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+const NavBar = () => {
+    return (
+        <div className="navbar">
+        <button>Home</button>
+        <button>Saved Jobs</button>
+        </div>
+    )
+}
+
+export default NavBar
