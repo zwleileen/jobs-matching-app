6f72e67d1e5666e457ad90ef8f15a3834b78fc06
eileen <zwleileen@gmail.com>
2025-01-25 00:40:44 +0800

    added more css

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -8,6 +8,9 @@ const SearchResults = (props) => {
         <ul>
         {props.searchResults.map((result) => (
             <li key={result.id}>
+                <button className="save-btn">
+                <i className="material-icons">favorite</i>
+                </button>
                 <h3>{result.company}</h3>
                 <p>{result.role}</p>
                 <p>{result.location}</p>
