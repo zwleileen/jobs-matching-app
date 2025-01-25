1991fcd83de340c6d03bc24ace7bea6fc2635103
eileen <zwleileen@gmail.com>
2025-01-25 11:28:09 +0800

    open correct link in new window

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,5 +1,9 @@
 const SearchResults = (props) => {
 
+    const handleClick = (link) => {
+        window.open(link, '_blank', 'noopener,noreferrer')
+    }
+
     return (
         <>
         {!props.searchResults.length ? (
@@ -7,7 +11,7 @@ const SearchResults = (props) => {
         ) : (
         <ul>
         {props.searchResults.map((result) => (
-            <li key={result.id}>
+            <li key={result.id} onClick={() => handleClick(result.link)} style={{ cursor: 'pointer' }}>
                 <button className="save-btn">
                 <i className="material-icons">favorite</i>
                 </button>
